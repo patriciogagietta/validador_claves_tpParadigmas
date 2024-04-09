c75179e08a1c38467ecf0a58bd3cf3d6a4822b35
patriciogagietta1 <patoxxgagietta1@gmail.com>
2024-04-09 17:11:47 -0300

    add symbols

--- js/validador.js
+++ js/validador.js
@@ -9,7 +9,7 @@ class Automata{
     letrasMinusculas = "abcdefghijklmnopqrstuvwxyz".split("")
     letrasMayusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")
     numeros = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-    simbolos_array = [ "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "`", "~", "-", "=", "[", "]", "{", "}", ";", ":", "'", '"', ",", ".", "<", ">", "/", "?", "|", "\\"]
+    simbolos_array = ["!", "¡", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "`", "~", "-", "=", "[", "]", "{", "}", ";", ":", "´", '"', ",", ".", "<", ">", "/", "?", "¿", "|", "\\", "°", "'", " "]
 
     validacion(contraseña){
         let estadoActual = 'I';
